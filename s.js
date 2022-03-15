6ca0a0c65d929f4e701e07d231cd916621579935
Yuri Kirsanov <iurykirsanow@yandex.ru>
2022-03-15 14:23:57 +0700

    Виджет button

--- s.js
+++ s.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
 
-   $("#datepicker").datepicker();
-   $("#datepicker1").datepicker({
-      monthNames:["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
-      dayNamesMin:["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
-      firstDay:1,
-      dateFormat:"dd.mm.yy"
-   });
+   $("#but1,#but2,#but3,#but4").button();
+   $("#group1,#group2").buttonset();
 
 });
