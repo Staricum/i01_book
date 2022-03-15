1d2b563f746ea9dc3314a6d816120fb23ca46c5f
Yuri Kirsanov <iurykirsanow@yandex.ru>
2022-03-15 14:17:45 +0700

    Виджет autocomplete

--- s.js
+++ s.js
@@ -1,20 +1,5 @@
-$(document).ready(function(){
-
-$(document).ready(function(){
-
-$("#el1").resizable().draggable({revert:true});
-
-$("#sort1").sortable();
-
-$("#el2,#el3").draggable({revert:true});
-
-$("#drop1").droppable({accept:"#el2,#el3",
-drop:function(event,ui){ui.draggable.css("display","none");
-$("#drop1").css("background-color","green");}});
-
-$("#drop2").droppable({accept:"#el3", drop:function(){
-$("#el3").css("display","none");
-$("#drop2").css("background-color","green");}});
-});
+$(document).ready(function() {
+  
+   $("#auto1").autocomplete({source:["Дмитрий","Мария","Владимир","Алексей","Екатерина","Олег","Ольга"]});
 
 });
